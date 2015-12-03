e1636ccf145e7efe752dc8846d5fbac0e2bda08c
Eric Holscher <eric@ericholscher.com>
2015-12-03 10:09:59 -0800

    Update to use the Read the Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'contents'
+master_doc = 'index'
 
 # General information about the project.
 project = u'Swift'
@@ -92,19 +92,14 @@ highlight_language = 'swift'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'sphinx_rtd_theme'
 
-html_style = 'swift.css'
+#html_style = 'swift.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  # Red links are a bit too garish
-  "linkcolor" : "#577492",
-  "visitedlinkcolor" : "#577492",
-  "hoverlinkcolor" : "#551A8B"
-}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["."]
